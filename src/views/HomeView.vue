52686a9e000ccb7b06849f06488174968e2184ab
btbrad <24836323+btbrad@users.noreply.github.com>
2025-05-12 21:59:44 +0800

    chore: 整合element-plus

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Home</h1>
+    <el-button type="success"><i-ep-SuccessFilled />Success</el-button>
+    <el-button type="info"><i-ep-InfoFilled />Info</el-button>
+    <el-button type="warning"><i-ep-WarningFilled />Warning</el-button>
+    <el-button type="danger"><i-ep-WarnTriangleFilled />Danger</el-button>
+  </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,56 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+
+import path from 'path'
+
+const pathSrc = path.resolve(__dirname, 'src')
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    vueDevTools(),
+    // vueDevTools(),
+    AutoImport({
+      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+      imports: ['vue'],
+      eslintrc: {
+        enabled: true, // 是否自动生成 eslint 规则，建议生成之后设置 false
+        filepath: './.eslintrc-auto-import.json', // 指定自动导入函数 eslint 规则的文件
+      },
+      dts: path.resolve(pathSrc, 'types', 'auto-imports.d.ts'), // 指定自动导入函数TS类型声明文件路径
+      resolvers: [
+        ElementPlusResolver(),
+        // 自动导入图标组件
+        IconsResolver({}),
+      ],
+      vueTemplate: true, // 是否在 vue 模板中自动导入
+    }),
+    Components({
+      dts: path.resolve(pathSrc, 'types', 'components.d.ts'), // 指定自动导入组件TS类型声明文件路径
+      resolvers: [
+        ElementPlusResolver(),
+        // 自动注册图标组件
+        IconsResolver({
+          enabledCollections: ['ep'], // element-plus图标库，其他图标库 https://icon-sets.iconify.design/
+        }),
+      ],
+    }),
+    Icons({
+      // 自动安装图标库
+      autoInstall: true,
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
